600183387b8051f193707379c9b653d80ef3ec71
Mohammed Mahdi <djaballah.mohammed.mahdi@gmail.com>
2024-09-15 06:11:36 +0100

    Created the base class and methods wanted

---
+++ BST.py
@@ -0,0 +1,34 @@
+class BST():
+    def __inti__(self, data):
+        self.data = data
+        self.right = None
+        self.left = None
+
+    def add_child(self, data):
+        # /////////
+        return None
+
+    def build_tree(elements):
+
+        return None
+
+
+    def in_order_traversal(self):
+
+        return None
+
+    def pre_order_traversal(self):
+
+        return None
+    
+    def post_order_traversal(self):
+
+        return None
+    
+    def search(self, val):
+
+        return None
+
+    def delete(self, val):
+
+        return None
