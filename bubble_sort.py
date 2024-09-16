58e2eddaaaac88583f8ca4c8966f0422664eb955
Mohammed Mahdi <djaballah.mohammed.mahdi@gmail.com>
2024-09-16 14:10:34 +0100

    added a bubble sort by key, it takes a dictionary and sorts its elements based on a provided key

--- bubble_sort.py
+++ bubble_sort.py
@@ -14,3 +14,21 @@ def bubble_sort(elements):
         if not swapped:
             break
     return elements
+
+
+def bubble_sort_by(elements, key):
+    size = len(elements) - 1
+
+    for i in range(size):
+        swapped = False
+
+        for j in range(size):
+            
+            if elements[j][key] > elements[j+1][key]:
+                elements[j], elements[j+1] = elements[j+1], elements[j]
+                swapped = True
+
+        if not swapped:
+            break
+    return elements
+
