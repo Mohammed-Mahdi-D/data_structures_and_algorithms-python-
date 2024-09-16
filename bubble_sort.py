807d1afeeff49b45ce4c49c5be8fa69e993aee39
Mohammed Mahdi <djaballah.mohammed.mahdi@gmail.com>
2024-09-16 14:05:56 +0100

    created simple sorting algorithm with python, bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(elements):
+    size = len(elements) - 1
+
+    for i in range(size):
+        swapped = False
+
+        for j in range(size):
+            
+            if elements[j] > elements[j+1]:
+                elements[j], elements[j+1] = elements[j+1], elements[j]
+                swapped = True
+        
+
+        if not swapped:
+            break
+    return elements
