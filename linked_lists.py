0044f6ad3031af4dc153ee412713136550ee4389
Mohammed Mahdi <djaballah.mohammed.mahdi@gmail.com>
2024-09-18 14:23:58 +0100

    Created methods to add a node to the end or to the beginning of the list =(push_front or push_back)

--- linked_lists.py
+++ linked_lists.py
@@ -6,4 +6,26 @@ class Node:
     def getnode(self):
         return self.data
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
+    def push_front(self, data):
+        self.head = Node(data, self.head)
+        return self.head
+
+    def push_back(self, data):
+        if(self.head == None):
+            self.head = Node(data, None)
+            return self.head
+        else:
+            itr = self.head
+            while True:
+                if itr.next == None:
+                    itr.next = Node(data, None)
+                    break
+                else:
+                    itr = itr.next
+                    continue
+            return itr.next
+        
